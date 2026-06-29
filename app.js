e37989708118ecac8d628aaf34073d96fc95b8f3
github7733a <143871345+github7733a@users.noreply.github.com>
2026-06-29 14:04:15 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1187,9 +1187,7 @@ function formatDateTime(iso){
 
   return d.getFullYear()+"/"
     +String(d.getMonth()+1).padStart(2,"0")+"/"
-    +String(d.getDate()).padStart(2,"0")+" "
-    +String(d.getHours()).padStart(2,"0")+":"
-    +String(d.getMinutes()).padStart(2,"0");
+    +String(d.getDate()).padStart(2,"0");
 }
 
 let goalSwipe = null;
